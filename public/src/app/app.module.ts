57bf020da153ee6fc5c33d322310ede0995196e4
Mohamed Koanda <m.koanda@aol.com>
2018-09-24 18:59:04 -0700

    document editor page frontend done

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { DocumentComponent } from './components/document/document.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ProfileComponent
+    ProfileComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule,
